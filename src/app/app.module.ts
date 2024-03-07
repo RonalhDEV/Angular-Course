92d4c37a89ccdd43760e33bd17f6fd60d1def2e9
Ronaldo <arolpersonal@gmail.com>
2024-03-06 20:39:02 -0500

    Fin de la sección 4 (1ra sobre Angular)

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/counter.module';
+import { HeoresModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeoresModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
